977445af642bc0d7e023b0fbe5045ba832f5bc18
Chi Cuong Tang <chicuong.tang01@gmail.com>
2022-11-10 17:34:17 +0700

    fix image link

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,15 +5,20 @@ export default function Card(props) {
         <div className="card">
             <div className="card--row">
                 <div className="card--image-div">
-                    <img src={`../images/${props.img}`} alt={props.img} />
+                    <img src={`images/${props.img}`} alt={props.img} />
                 </div>
                 <div className="card--title-div">
                     <p className="card--location">
-                        <img src="../images/location.svg" alt="location-logo" />
+                        <img src="images/location.svg" alt="location-logo" />
                         <span className="card--location-name text">
                             {props.location}
                         </span>
-                        <a href={props.googleMapsUrl} className="text" target='_blank' rel='noopener noreferer'>
+                        <a
+                            href={props.googleMapsUrl}
+                            className="text"
+                            target="_blank"
+                            rel="noopener noreferer"
+                        >
                             View on Google Maps
                         </a>
                     </p>
